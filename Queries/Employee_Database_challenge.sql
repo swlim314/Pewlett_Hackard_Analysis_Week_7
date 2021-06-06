7c86c7d4f7c5ae8e06501665633fd0c4272d3f21
swlim314 <81394970+swlim314@users.noreply.github.com>
2021-06-06 19:40:01 -0400

    Update Employee_Database_challenge.sql

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -40,10 +40,10 @@ SELECT DISTINCT ON(e.emp_no) e.emp_no,
 	   ti.title
 --INTO mentorship_eligibility
 FROM employees as e
-INNER JOIN dept_emp as de
+LEFT JOIN dept_emp as de
 ON (e.emp_no = de.emp_no)
-INNER JOIN titles as ti
+LEFT JOIN titles as ti
 ON (e.emp_no = ti.emp_no)
 WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
-AND (de.to_date = '9999-01-01')
+AND (ti.to_date = '9999-01-01')
 ORDER BY emp_no
